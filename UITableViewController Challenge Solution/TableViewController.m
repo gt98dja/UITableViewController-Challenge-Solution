0bfa4c88495c503a963d7f530092b04fbcce1c5e
Daniel Jansson <danja612@MacBook-Pro.local>
2014-04-16 14:37:33 +0200

    added sections and rows and some data to the cells

--- UITableViewController Challenge Solution/TableViewController.m
+++ UITableViewController Challenge Solution/TableViewController.m
@@ -46,26 +46,54 @@
 {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    switch (section) {
+        case 0:
+            return 2;
+            break;
+        case 1:
+            return 1;
+            break;
+        case 2:
+            return 3;
+            break;
+            
+        default:
+            return 0;
+            break;
+    }
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     
     // Configure the cell...
     
+    switch (indexPath.section) {
+        case 0:
+            cell.textLabel.text = @"I am in section 0";
+            break;
+        case 1:
+            cell.textLabel.text = @"another section";
+            break;
+        case 2:
+            cell.textLabel.text = [NSString stringWithFormat:@"Cell %i", indexPath.row];
+            break;
+            
+        default:
+            break;
+    }
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
