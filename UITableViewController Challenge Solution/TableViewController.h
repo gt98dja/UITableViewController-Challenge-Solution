7a77edbac12dd5b0418db4ce932e41cad874df10
Daniel Jansson <danja612@MacBook-Pro.local>
2014-04-16 14:05:35 +0200

    added a TableViewController class

--- UITableViewController Challenge Solution/TableViewController.h
+++ UITableViewController Challenge Solution/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Daniel Jansson on 2014-04-16.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface TableViewController : UITableViewController
 
 @end
